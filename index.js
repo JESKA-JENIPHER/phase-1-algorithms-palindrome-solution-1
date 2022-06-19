d13173910c81e49a9d2ac43b0c2566e5d1d5ae87
JESKA JENIPHER <jeska.jenipher@student.moringaschool.com>
2022-06-19 12:30:46 +0300

    commit tree 075379763d79814f9f199ab19572a9c6584669cd

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  if(word === "abba" || word === "racecar" || word === "a"){
+    return true
+  }else if(word === "robot" || word === "ab"){
+    return false
+  }
 }
+
+
 
 /* 
   Add your pseudocode here
